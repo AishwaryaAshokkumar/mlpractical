d07ac1e6be9fbe76f94d7d42096de48c04d0d6ea
Aishwarya A <aishwarya11235@gmail.com>
2023-11-29 00:15:28 +0000

    VGG_38 + BN results

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -341,8 +341,8 @@ class ConvolutionalNetwork(nn.Module):
         
 class ConvolutionalProcessingBlock_BN(nn.Module):
     def __init__(self, input_shape, num_filters, kernel_size, padding, bias, dilation):
-        super(ConvolutionalProcessingBlock, self).__init__()
-
+        super().__init__()
+        
         self.num_filters = num_filters
         self.kernel_size = kernel_size
         self.input_shape = input_shape
